b94d564f317236b1b771f90be87233d0b6254a7f
najibean <najibullah89@gmail.com>
2021-04-21 02:40:11 +0700

    add file .env, add func for env default value

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,9 +16,15 @@ type Server struct {
 	Router	*mux.Router
 }
 
+type AppConfig struct {
+	Name	string
+	Env		string
+	Port 	string
+}
+
 // memanggil routes -- kenapa tidak dimaksudkan untuk para router saja ya?
-func (server *Server) Initialize() {
-	fmt.Println("Welcome to Raja Kado & Boutique")
+func (server *Server) Initialize(appConfig AppConfig) {
+	fmt.Println("Welcome to " + appConfig.Name)
 
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
@@ -28,10 +36,33 @@ func (server *Server) Run(port string) {
 	log.Fatal(http.ListenAndServe(port, server.Router))
 }
 
+// membuat func nilai default .env jika data didalamnya kosong. Karena di Go jika data hilang, server port masih jalan
+func getEnv(key, defaultnya string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return defaultnya
+}
+
 // menjalankan semua fungsi yang ada diatas
 func Run() {
 	var server = Server{}
+	var appConfig = AppConfig{}
 
-	server.Initialize()
-	server.Run(":9000")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error on loading .env file")
+	}
+
+	// appConfig.Name = os.Getenv("APP_NAME")
+	// appConfig.Env = os.Getenv("APP_ENV")
+	// appConfig.Port = os.Getenv("APP_PORT")
+
+	// tes fungsi jika data di .env tidak ada
+	appConfig.Name = getEnv("APP_NAME", "waroeng boutique")
+	appConfig.Env = getEnv("APP_ENV", "environment")
+	appConfig.Port = getEnv("APP_PORT", "9989")
+
+	server.Initialize(appConfig)
+	server.Run(":" + appConfig.Port)
 }
